ad31ba9779bac3a9f86f6dd1afb7a81f03a73717
unknown <vishalreddypasula@gmail.com>
2024-12-06 17:48:31 +0530

    if else voting and age eligibility task

---
+++ index.js
@@ -0,0 +1,53 @@
+var userinput=prompt("enter your age here")
+if (userinput>=18){
+    console.log("you are eligible to vote");
+}else{
+  console.log("you are not eligible to vote");
+}
+ans:Age should be more than or equal to 18 should eligible to vote (print if statement)/ Below 18 not eligible to vote (print else statement)
+
+
+var gender=prompt("enter your gender")
+var age=prompt("enter your age")
+if (gender=='male'){
+    if (age>=21){
+    console.log("your are eligible for marriage")
+    }else{
+    console.log("your are not eligible for marriage")
+    }
+} else{
+    if (age>=18){
+    console.log("your are eligible for marriage")
+    }else{
+    console.log("your are not eligible for marriage")
+    }
+}
+ ans:Age should be more than or equal to 21 for male should eligible for marriage/ Age should be more than or equal to 18 for female should eligible for marriage
+
+
+var age=prompt("enter your age here")
+if (age>=18){
+    console.log("you are eligible for driving license ")
+    }else{
+        console.log("you are not eligible for driving license ") 
+    }
+ ans:Age should be more than or equal to 18 should eligible for driving license (print if statement)/ Below 18 not eligible for driving license(print else statement)
+
+var percentage=prompt("enter your percentage")
+
+if(percentage>=75){
+      console.log("you are eligible to appear for exam ")
+    }else{
+        console.log("you are not eligible to appear for exam ") 
+    }
+    
+ ans:The percentage should be greater than or equal to 75 he or she can attend the exam (print if statement)/Below 75 cannot be attend the exam(print else statement)
+
+var age=prompt("enter your age here")
+
+if (age>=60){
+    console.log("Your are eligible for discount")
+}else{
+     console.log("Your are not eligible for discount")
+}
+
